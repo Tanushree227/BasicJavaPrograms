2e6836466582ada78ceb0dbd96bcdfc13eccbbf5
Tanushree227 <tanushreedas813@gmail.com>
2025-06-03 15:08:57 +0530

    .

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class SwapNumbers {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
